e8c7496eae59c51ab0784b2f22e62df7ee2feed5
pawelk5 <pawel.kromka@proton.me>
2025-05-02 20:11:34 +0200

    fix typo

--- lib/test/ListTest.cpp
+++ lib/test/ListTest.cpp
@@ -5,5 +5,5 @@ TEST(List, BaseConstructor) {
     List board("test");
    
     EXPECT_EQ(board.GetName(), "test");
-    EXPECT_EQ(board.GetCards().size(), 3);
+    EXPECT_EQ(board.GetCards().size(), 0);
 }
