b37919f28f38f5248e6aaf3bd69e3d42ca5509e2
Kamil Gargula <kamil.gargula256@gmail.com>
2025-05-05 15:57:57 +0200

    add: script for live reload

---
+++ live_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Function to build and test
+build_and_test() {
+    mkdir -p build
+    cd build
+    cmake -B . -S ..
+    make 
+    # --parallel
+    make test
+    cd ..
+}
+
+# Watch for changes and rebuild
+while true; do
+    build_and_test
+    inotifywait -e modify,create,delete -r .
+done
