e7df0a8c97c9fd97dcfbf2563e6eedbecb23d5a8
pawelk5 <pawel.kromka@proton.me>
2025-05-06 10:48:05 +0200

    move unit tests to unit directory

--- lib/test.cmake
+++
@@ -1,8 +0,0 @@
-include(GoogleTest)
-
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tests)
-
-enable_testing()
-add_subdirectory("test")
-
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
